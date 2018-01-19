38f1b9ff9c6cc73b332fb4231b57841d644b4319
Josh <jjbdlsantos@gmail.com>
2018-01-19 19:55:37 +1100

    Implemented bullet spawner mouse tracking

--- Assets/Scripts/MouseTracking.cs
+++ Assets/Scripts/MouseTracking.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour {
+public class MouseTracking : MonoBehaviour {
+
+    private Transform spawnerTransform;
+    private Vector3 mousePos;
+
+    private float rotateSpeed = 5f;
+    private float radius = 0.6f;
+    private Vector3 _centre;
+    private float _angle;
+
+    public float angle;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        spawnerTransform = GetComponent<Transform>();
+        _centre = transform.localPosition;
+        angle = 0;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //raw input mouse position (0,0 @ lower left corner)
+        Vector3 pos = Input.mousePosition;
+        //converts input.mouseposition into position relative to main camera
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float a1 = Mathf.Atan2(transform.position.y - mousePos.y, transform.position.x - mousePos.x) * 180 / Mathf.PI;
+        
+        _angle = -(90f + a1) * Mathf.PI/180;
+
+        var offset = new Vector3(Mathf.Sin(_angle), Mathf.Cos(_angle), 0) * radius;
+        transform.localPosition = _centre + offset;
+    }
 }
