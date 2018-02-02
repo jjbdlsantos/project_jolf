db4c485b8ce504d928b5735bdbafc5696192cc1e
ND\Andy <andy.go@student.uts.edu.au>
2018-02-02 19:19:29 +1100

    They see me rollin, they hatin

---
+++ Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPhysics : MonoBehaviour {
+
+    public float multiplier = 100f;
+    private LineRenderer mouseLine;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos;
+        Vector3 startPos = new Vector3 (0,0,0);
+        Vector3 endPos = new Vector3(0, 0, 0);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (mouseLine == null)
+            {
+                CreateLine();
+            }
+
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 5f));
+            startPos = mousePos;
+
+            mouseLine.SetPosition(0, mousePos);
+            mouseLine.SetPosition(1, mousePos);
+        }
+
+        else if (Input.GetMouseButton(0) && mouseLine)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 5f));
+            mouseLine.SetPosition(1, mousePos);
+        }
+
+        else if (Input.GetMouseButtonUp(0) && mouseLine)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 5f));
+            endPos = mousePos;
+
+            mouseLine.SetPosition(1, mousePos);
+
+            ScalarCalculation(startPos, endPos);
+            Destroy(mouseLine);
+        }
+    }
+
+    private void CreateLine()
+    {
+        mouseLine = new GameObject("Line").AddComponent<LineRenderer>();
+        mouseLine.material = new Material(Shader.Find("Particles/Additive"));
+        mouseLine.GetComponent<LineRenderer>().startColor = Color.red;
+        mouseLine.GetComponent<LineRenderer>().endColor = Color.red;
+        mouseLine.sortingLayerName = "Foreground";
+
+        mouseLine.positionCount = 2;
+        mouseLine.startWidth = 0.05f;
+        mouseLine.endWidth = 0.05f;
+        mouseLine.useWorldSpace = true;
+    }
+
+    private void ApplyForce(float xForce, float yForce)
+    {
+        Rigidbody rb;
+        var player = GameObject.Find("Player");
+        rb = player.GetComponent<Rigidbody>();
+        Debug.Log("xForce: " + xForce + " zForce: " + yForce);
+        rb.AddForce(xForce, yForce, 0, ForceMode.Force);
+    }
+
+    private void ScalarCalculation(Vector3 start, Vector3 end)
+    {
+        float xDiff = Mathf.Abs(start.x - end.x);
+        float yDiff = Mathf.Abs(start.y - end.y);
+
+        float xSign = (start.x - end.x) / xDiff;
+        float ySign = (start .y - end.y) / yDiff;
+
+        ApplyForce((xSign * xDiff * multiplier), (ySign * yDiff * multiplier));
+    }
+}
